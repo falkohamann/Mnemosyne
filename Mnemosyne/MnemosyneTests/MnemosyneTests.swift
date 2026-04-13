04686eddcfac0de153f1450182b1a6cf6925cbca
Falko Hamann <falko.hamann@sap.com>
2026-04-13 14:30:53 +0200

    chore: add unit test target with placeholder test

---
+++ Mnemosyne/MnemosyneTests/MnemosyneTests.swift
@@ -0,0 +1,15 @@
+//
+//  MnemosyneTests.swift
+//  MnemosyneTests
+//
+//  Created by Hamann, Falko on 13.04.26.
+//
+
+import XCTest
+@testable import Mnemosyne
+
+final class ClipboardItemTests: XCTestCase {
+    func test_placeholder() {
+        XCTAssertTrue(true)
+    }
+}
